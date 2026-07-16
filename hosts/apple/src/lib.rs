62016785dcb435e19d7ce1aafa024bbf494e2f5b
AAswordman <1002153674@qq.com>
2026-07-16 14:38:09 +0800

    feat(macos): add CLI release build support

--- hosts/apple/src/lib.rs
+++ hosts/apple/src/lib.rs
@@ -1,12 +1,17 @@
 #![allow(non_snake_case)]
 
+#[cfg(target_os = "macos")]
+#[path = "../../common/chromium_browser.rs"]
+pub mod chromium_browser;
 pub mod tools;
 
+pub use operit_host_native_common::NativeHostRuntimeEventSchedulerHost as AppleHostRuntimeEventSchedulerHost;
 pub use tools::audio::{AppleAudioPlaybackHost, AppleMusicCommand};
 pub use tools::bluetooth::AppleBluetoothHost;
+#[cfg(target_os = "macos")]
+pub use tools::browser::{AppleBrowserAutomationHost, AppleWebVisitHost};
 pub use tools::fs::AppleFileSystemHost;
 pub use tools::host_runtime_event::AppleHostRuntimeEventHost;
-pub use operit_host_native_common::NativeHostRuntimeEventSchedulerHost as AppleHostRuntimeEventSchedulerHost;
 pub use tools::http::AppleHttpHost;
 pub use tools::local_inference::{AppleLocalInferenceCommand, AppleLocalInferenceHost};
 pub use tools::runtime::AppleManagedRuntimeHost;
